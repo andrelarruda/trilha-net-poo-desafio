b901d0fc7da7c35c51251c18f18194e1886ad58f
Andre Arruda <andrebass27@gmail.com>
2024-01-09 01:05:48 -0300

    Implementacao do desafio + testes.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia("1234", "Nokia 2200", "98239823", 12);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+System.Console.WriteLine();
+
+System.Console.WriteLine("iPhone");
+Smartphone iphone = new Iphone("4321", "iPhone 12 Pro Max", "298348", 512);
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("Snapchat");
+iphone.InstalarAplicativo("Opera");
